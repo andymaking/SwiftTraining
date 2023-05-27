242db290eba94e7d97684218b72682866be5226a
Andima Udoh <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>
2023-05-27 07:08:02 +0100

    Initial Commit

---
+++ tunde_class_one/tunde_class_oneApp.swift
@@ -0,0 +1,17 @@
+//
+//  tunde_class_oneApp.swift
+//  tunde_class_one
+//
+//  Created by Andima Udoh on 27/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct tunde_class_oneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
